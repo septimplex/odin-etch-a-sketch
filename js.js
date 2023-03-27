98a42a51e8621ede0912f791b7652405d4bf4312
Septimplex <septimplexx@gmail.com>
2023-03-27 22:50:40 +0200

    add resetGrid function

--- js.js
+++ js.js
@@ -1,15 +1,64 @@
 const grid = document.querySelector("#grid-container");
+const size = document.querySelector("#h3-size");
 
 
+const input = document.querySelector("#input");
+input.addEventListener('input', function(){ 
+        size.innerHTML = this.value + " x " + this.value;
 
-for(let i=0;i<2;i++){
-    for(let j=0;j<2;j++){
-        const box = document.createElement("div");
-        box.classList = "box";
-        grid.appendChild(box);
-        
+     });
+
+
+createGrid(input.value);
+
+const btn_black = document.querySelector("#black");
+const divs = document.querySelectorAll(".box");
+const btn_reset = document.querySelector("#reset");
+
+btn_reset.addEventListener("click", resetGrid);
+
+for(let i=0;i<divs.length;i++){
+    divs[i].addEventListener("mouseover",function(e){ 
+        e.target.classList = "box toBlack";  
+    });
+}
+
+function resetGrid(){
+    for(let i=0;i<divs.length;i++){
+        divs[i].classList = "box";
     }
 }
 
-grid.style.width = "960/2";
-grid.style.height = "960/2";
+
+
+
+function createGrid(x){
+    grid.innerHTML = "";
+    for(let i=0;i<x;i++){
+        for(let j=0;j<x;j++){
+            const box = document.createElement("div");
+            box.classList = "box";
+            grid.appendChild(box);       
+        }  
+    }
+    
+    
+    const gridSize = 550/x + "px";
+    const boxSize = document.querySelectorAll(".box");
+    for(let i=0;i<boxSize.length;i++){
+            boxSize[i].style.width = gridSize;
+            boxSize[i].style.height = gridSize;
+    } 
+    
+}
+
+
+
+
+
+
+
+
+
+
+
