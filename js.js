326f05933d95b1ed7a59b926765c067cbd4c1dbf
Septimplex <septimplexx@gmail.com>
2023-03-28 22:00:54 +0200

    add functions for buttons to return different colors

--- js.js
+++ js.js
@@ -11,26 +11,76 @@ input.addEventListener('input', function(){
 
 createGrid(input.value);
 
-const btn_black = document.querySelector("#black");
 const divs = document.querySelectorAll(".box");
+const btn_black = document.querySelector("#black");
+const btn_random = document.querySelector("#random");
+const btn_shade = document.querySelector("#shade");
 const btn_reset = document.querySelector("#reset");
 
 btn_reset.addEventListener("click", resetGrid);
 
-for(let i=0;i<divs.length;i++){
-    divs[i].addEventListener("mouseover",function(e){ 
-        e.target.classList = "box toBlack";  
-    });
+btn_black.addEventListener("click", activateBlack);
+btn_random.addEventListener("click", activateRandom);
+btn_shade.addEventListener("click", activateShade);
+
+
+    
+
+
+
+function activateRandom(){
+    btn_black.classList = "button";
+    btn_shade.classList = "button";
+    btn_random.classList = "button active";
+
+        for(let i=0;i<divs.length;i++){
+            divs[i].addEventListener("mouseover",function(e){ 
+                e.target.style.backgroundColor = getColor();  
+                });
+            }
+    
+}
+
+function activateBlack(){
+    btn_black.classList = "button active";
+    btn_shade.classList = "button";
+    btn_random.classList = "button";
+
+    for(let i=0;i<divs.length;i++){
+        divs[i].addEventListener("mouseover",function(e){ 
+            e.target.style.backgroundColor = "black";  
+            });
+        }
+           
+}
+
+function activateShade(){
+    btn_black.classList = "button";
+    btn_shade.classList = "button active";
+    btn_random.classList = "button";
+
+    let counter = 0;
+    for(let i=0;i<divs.length;i++){
+        divs[i].addEventListener("mouseover",function(e){ 
+            
+            if(counter < 9){
+                e.target.style.backgroundColor = shadeList[counter];
+                counter++;
+            }
+            else{
+                e.target.style.backgroundColor = shadeList[counter];
+                counter = 0;
+                
+            }
+            }); 
+        }
 }
 
 function resetGrid(){
     for(let i=0;i<divs.length;i++){
-        divs[i].classList = "box";
+        divs[i].style.backgroundColor = "hsl(347, 100%, 97%)";
     }
 }
-
-
-
 
 function createGrid(x){
     grid.innerHTML = "";
@@ -50,6 +100,19 @@ function createGrid(x){
             boxSize[i].style.height = gridSize;
     } 
     
+}
+
+//find out shade
+let shadeList = ["hsl(0,0%,90%)","hsl(0,0%,80%)","hsl(0,0%,70%)",
+                 "hsl(0,0%,60%)","hsl(0,0%,50%)","hsl(0,0%,40%)",
+                 "hsl(0,0%,30%)","hsl(0,0%,20%)","hsl(0,0%,10%)",
+                 "hsl(0,0%,0%)"];
+
+//list of random colors
+let colorList = ["#f23f27","#3bbbed","#FCB13B","#f2ec6f","#F990ED","#c184f0","#2dcf58"];
+
+function getColor(){
+    return colorList[Math.floor(Math.random() * colorList.length)];
 }
 
 
